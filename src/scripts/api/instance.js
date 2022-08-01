a4d0429eb10f0e0b18b30f4747f90d4075569113
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-08-01 17:50:00 +0300

    edit instance.js

--- src/scripts/api/instance.js
+++ src/scripts/api/instance.js
@@ -1,18 +1,20 @@
 const instance = axios.create({
 	baseURL: 'https://ajax.test-danit.com/api/v2/cards',
+	headers: { 'Content-Type': 'application/json;charset=utf-8' },
 });
 
 instance.interceptors.request.use(config => {
-	// const token = localStorage.getItem('token');
-	// const token = getCookie('token')
+	const token = getCookie('token');
 
 	if (token) {
 		config.headers = {
-			// 'Authorization': `Bearer ${token}`
+			'Authorization': `Bearer ${token}`,
 		}
 	}
 
 	return config;
+}, error => {
+	return Promise.reject(error);
 })
 
 export default instance;
