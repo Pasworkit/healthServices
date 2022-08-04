3e7c48cfd6a4de80053392baddf9aae2382d4cda
Александр Паришкура <pasworkit@gmail.com>
2022-08-04 16:31:05 +0300

    fix check authentication

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,28 +4,32 @@ import login from "./api/login.js";
 const btnLogin = document.querySelector('.header__btn-login');
 const btnCreateVisit = document.querySelector('.header__btn-createVisit');
 
-btnLogin.addEventListener('click', () => {
-    const modal = new LoginModal();
-    modal.render();
+    if (document.cookie.includes('token')) {
+        btnLogin.classList.add('none');
+        btnCreateVisit.classList.remove('none');
+    } else {
+        btnLogin.addEventListener('click', () => {
+            const modal = new LoginModal();
+            modal.render();
 
-    const btnRegistration = document.querySelector('.btn-registration');
+            const btnRegistration = document.querySelector('.btn-registration');
 
-    btnRegistration.addEventListener('click', () => {
-        const email = document.querySelector("#exampleInputEmail1").value;
-        const password = document.querySelector("#exampleInputPassword1").value;
-        login(email, password).then((r) => {
-            if (r < 300) {
-                modal.closeModal();
-                btnLogin.classList.add('none');
-                btnCreateVisit.classList.remove('none');
-            } else {
-                document.querySelector('.form-text').innerText = 'Неверный логин или пароль'
-            }
+            btnRegistration.addEventListener('click', () => {
+                const email = document.querySelector("#exampleInputEmail1").value;
+                const password = document.querySelector("#exampleInputPassword1").value;
+                login(email, password).then((r) => {
+                    if (r < 300) {
+                        modal.closeModal();
+                        btnLogin.classList.add('none');
+                        btnCreateVisit.classList.remove('none');
+                    } else {
+                        document.querySelector('.form-text').innerText = 'Неверный логин или пароль'
+                    }
 
-        });
-    })
-})
-
+                });
+            })
+        })
+    }
 
 
 
