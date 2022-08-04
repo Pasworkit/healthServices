03cbb96075861de23d2ee20ce01a467d32dd5ece
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-08-04 10:40:01 +0300

    import visit modal; add event listener on create visit button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,36 @@
 import Modal from "./classes/Modal.js";
 import login from "./api/login.js";
+import CreateVisitModal from "./classes/CreateVisitModal.js";
 
 const btnLogin = document.querySelector('.header__btn-login');
 const btnCreateVisit = document.querySelector('.header__btn-createVisit');
 
 btnLogin.addEventListener('click', () => {
-    const modal = new Modal();
-    modal.render();
+	const modal = new Modal();
+	modal.render();
 
-    const btnRegistration = document.querySelector('.btn-registration');
-    btnRegistration.addEventListener('click', () => {
-        const emailInput = document.querySelector(".email");
-        const passwordInput = document.querySelector(".password");
-        const email = emailInput.value;
-        const password = passwordInput.value;
-        login(email, password).then((r) => {
-            if (r < 300) {
-                modal.closeModal();
-                btnLogin.classList.add('none');
-                btnCreateVisit.classList.remove('none');
-            } else {
-                document.querySelector('.label-mail').innerText = 'Неверный логин или пароль'
-            }
+	const btnRegistration = document.querySelector('.btn-registration');
+	btnRegistration.addEventListener('click', () => {
+		const emailInput = document.querySelector(".email");
+		const passwordInput = document.querySelector(".password");
+		const email = emailInput.value;
+		const password = passwordInput.value;
+		login(email, password).then((r) => {
+			if (r < 300) {
+				modal.closeModal();
+				btnLogin.classList.add('none');
+				btnCreateVisit.classList.remove('none');
+			} else {
+				document.querySelector('.label-mail').innerText = 'Неверный логин или пароль'
+			}
 
-        });
-    })
+		});
+	})
 })
 
+btnCreateVisit.addEventListener('click', () => {
+	new CreateVisitModal().render();
+})
 
 
 
