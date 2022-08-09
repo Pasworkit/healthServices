94db1256825a3b98b3dc111552c7588b57de508c
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-08-09 20:13:14 +0300

    add event listeners to perform drag & drop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,10 @@ import createVisit from "./api/createVisit.js";
 
 import { showCards } from "./functions/showCards.js";
 
+import { handleDragStart, handleDragEnd, handleDragEnter, handleDragOver, handleDragLeave, handleDrop } from "./functions/handleDrags.js";
+
 const btnLogin = document.querySelector('.header__btn-login');
 const btnCreateVisit = document.querySelector('.header__btn-createVisit');
-
-
 
 if (document.cookie.includes('token')) {
 	toggleBtn();
@@ -22,3 +22,16 @@ btnCreateVisit.addEventListener('click', () => {
 })
 
 showCards();
+
+const cardsContainer = document.querySelector('.container-cards');
+
+cardsContainer.addEventListener('mousedown', (e) => {
+	document.querySelectorAll('.cards__card').forEach(card => {
+		card.addEventListener('dragstart', handleDragStart);
+		card.addEventListener('dragover', handleDragOver);
+		card.addEventListener('dragenter', handleDragEnter);
+		card.addEventListener('dragleave', handleDragLeave);
+		card.addEventListener('dragend', handleDragEnd);
+		card.addEventListener('drop', handleDrop);
+	});
+});
