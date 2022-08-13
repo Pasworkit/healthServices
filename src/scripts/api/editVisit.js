3ba4304c3e8ec2797e98639d71d23211511b2535
Evgeniy Yaloza <zyaloza@gmail.com>
2022-08-13 15:07:38 +0300

    fix editVisit

--- src/scripts/api/editVisit.js
+++ src/scripts/api/editVisit.js
@@ -1,7 +1,7 @@
 import instance from "./instance.js";
 import Card from "../classes/card.js";
 
-const editVisit = async (visit, id) => {
+const editVisit = async (visit, id, card) => {
     const { body } = visit;
     body["id"] = id;
     
@@ -9,8 +9,24 @@ const editVisit = async (visit, id) => {
         const { status } = await instance.put(`/${id}`, body);
         if (status === 200) {
             visit.closeModal();
-            document.querySelector(`.card-${id}`).remove();
-            new Card(body).render(document.querySelector(".container-cards"));
+            console.log(card);
+            const { fullName, doctor, status, title, description, urgency, cardiovascularDiseases, bloodPressure, bodyMassIndex, age, lastVisit} = body;
+            card.fullName = fullName;
+            card.doctor = doctor;
+            card.title = title;
+            card.description = description;
+            card.urgency = urgency;
+            card.status = status;
+            // card.id = id;
+            card.cardiovascularDiseases = cardiovascularDiseases;
+            card.bloodPressure = bloodPressure;
+            card.bodyMassIndex = bodyMassIndex;
+            card.age = age;
+            card.lastVisit = lastVisit;
+
+
+            // document.querySelector(`.card-${id}`).remove();
+            // new Card(body).render(document.querySelector(".container-cards"));
         }
     }
     catch (err) {
